4519d0e04cb39ec3a8f391b293899a657b180094
jinyanliu <jane.jinyan.liu@gmail.com>
2017-11-20 22:24:30 +0100

    Add comments.

--- app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -14,10 +14,20 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
+ * This test demos a user clicking the increment/decrement button and verifying that it properly
+ * increase/decrease the quantity the total cost.
+ * <p>
  * Created by jane on 17-11-20.
  */
 @RunWith(AndroidJUnit4.class)
 public class OrderActivityBasicTest {
+    /**
+     * The ActivityTestRule is a rule provided by Android used for functional testing of a single
+     * activity. The activity that will be tested will be launched before each test that's annotated
+     * with @Test and before methods annotated with @before. The activity will be terminated after
+     * the test and methods annotated with @After are complete. This rule allows you to directly
+     * access the activity during the test.
+     */
     @Rule
     public ActivityTestRule<OrderActivity> mActivityTestRule
             = new ActivityTestRule<>(OrderActivity.class);
@@ -33,7 +43,10 @@ public class OrderActivityBasicTest {
     }
 
     @Test
-    public void clickDecrementButton_ChangesQuantityAndCost(){
+    public void clickDecrementButton_ChangesQuantityAndCost() {
+        // Check the initial quantity variable is zero
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
+        // Click on decrement button
         onView(withId(R.id.decrement_button)).perform(click());
         onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
         onView(withId(R.id.cost_text_view)).check(matches(withText("$0.00")));
